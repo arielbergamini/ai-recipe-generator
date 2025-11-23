20a413d034091eda93b170839cf958f7b88832e6
arielbergamini <arielbergamini263@gmail.com>
2025-11-22 22:47:47 -0600

    more fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import { type FormEvent, useState } from "react";
+import type { FormEvent } from "react";
+import { useState } from "react";
 import { Loader, Placeholder } from "@aws-amplify/ui-react";
 import "./App.css";
 import { Amplify } from "aws-amplify";
-import { type Schema } from "../amplify/data/resource";
+import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
